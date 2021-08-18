5fb803ac64c5eab4dd2a33356ba0fe351aa80922
harbeerg20 <harveer.gurjar.9575@gmail.com>
2021-08-18 16:01:47 +0530

    1

--- quiz_mania/lib/main.dart
+++ quiz_mania/lib/main.dart
@@ -1,10 +1,16 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
 import 'package:quiz_mania/pages/start_page.dart';
 import 'package:quiz_mania/pages/que_page.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: QuePage(),
+    initialRoute: '/',
+    routes: {
+      '/': (context)=>StartPage(),
+      '/second':(context)=>QuePage(),
+    },
   ));
 }
 
--- quiz_mania/lib/pages/start_page.dart
+++ quiz_mania/lib/pages/start_page.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:quiz_mania/pages/que_page.dart';
 
 class StartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.deepOrangeAccent,
+    backgroundColor: Colors.blueGrey[100],
+     
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
@@ -13,7 +15,7 @@ class StartPage extends StatelessWidget {
            children: [
              SizedBox(width: 40.0,),
              Padding(
-               padding: EdgeInsets.all(100.0),
+               padding: EdgeInsets.fromLTRB(100.0, 50, 100, 20),
                child: Text(
                    'Quiz Mania',
                   style: GoogleFonts.permanentMarker(
@@ -23,23 +25,36 @@ class StartPage extends StatelessWidget {
              ),
            ],
          ),
-          SizedBox(height: 200.0,),
+        
+        CircleAvatar(
+          backgroundImage: NetworkImage('https://image.shutterstock.com/image-vector/quiz-icon-logo-260nw-1298743501.jpg'),
+          radius: 180.0,
+        ),
+          SizedBox(height: 50.0,),
           Center(
-            child: ElevatedButton.icon(
-              onPressed: (){},
-              label: Text('Start Quiz',
-              style: TextStyle(
-                fontSize: 30.0,
-                color: Colors.greenAccent,
-              ),
-              ),
-              icon: Icon(Icons.restart_alt,
-              size: 30.0,
-                color: Colors.greenAccent,
-              ),
+            child: Ink.image(image:NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbcemnjjhnROMD-EzV_-QSAEQ-G7DyxmzfCA&usqp=CAU.jpg'),
+            fit: BoxFit.fill,
+            height: 80.0,
+            width: 200,
+            child: InkWell(
+              onTap: (){
+                print('bhak bsdk');
+                Navigator.pushNamed(context, '/second');
+              },
+            ),
+            ),
+              // icon: Image(
+              //   image: NetworkImage('https://image.shutterstock.com/image-vector/quiz-icon-logo-260nw-1298743501.jpg'),
+              //   width: 30.0,
+              //   height: 30,
+              //   ),
+            
+            
+              
+            
 
             ),
-          ),
+          
         ],
       ),
     );
